462d1e412afcdfbe39d8d7c0b1f8333f0a19bae4
cgarling <chris.t.garling@gmail.com>
2025-12-01 22:27:20 -0500

    Start working on internals of EGG

--- src/GalaxyGenerator.jl
+++ src/GalaxyGenerator.jl
@@ -2,13 +2,16 @@ module GalaxyGenerator
 using ArgCheck: @argcheck, @check
 using Compat: logrange
 using DataInterpolations: AkimaInterpolation
+using Distributions: LogNormal, Normal, Uniform, mean
 using IrrationalConstants: logten
-import Random
+using Random: Random, default_rng, AbstractRNG
 using Trapz: trapz
+using SpecialFunctions: erf
 
 export SchechterMassFunction, DoubleSchechterMassFunction
 
 include("random.jl")
 include("Schechter.jl")
+include("EGG.jl")
 
 end
