241672e6f9aadc52b19d88db6ac32eef4d0f1f48
cgarling <chris.t.garling@gmail.com>
2025-12-03 16:52:26 -0500

    revise sizes, add SFRs, IR properties

--- src/EGG/EGG.jl
+++ src/EGG/EGG.jl
@@ -12,6 +12,7 @@ using DataInterpolations: LinearInterpolation
 using Distributions: LogNormal, Normal, Uniform
 using FITSIO: FITS
 using IrrationalConstants: logten
+using PhotometricFilters: AbstractFilter, magnitude
 using Random: Random, default_rng, AbstractRNG
 using SpecialFunctions: erf
 using StaticArrays: SVector
@@ -53,7 +54,7 @@ Returns U-V and V-J colors `(uv, vj)`.
 #         (uv, vj)
 #     end
 # end
-function uv_vj(logMstar, z, SF::Bool; rng::AbstractRNG=default_rng())
+function uv_vj(logMstar, z, SF::Bool, ; rng::AbstractRNG=default_rng())
     # This implementation is based on the code in the egg repository
     uv, vj = if SF
         a0 = 0.58 * erf(logMstar - 10) + 1.39
@@ -88,7 +89,7 @@ function uv_vj(logMstar, z, SF::Bool; rng::AbstractRNG=default_rng())
 end
 
 # SF is whether galaxy is star-forming or not
-function egg(Mstar, z, SF::Bool; 
+function egg(Mstar, z, SF::Bool, @nospecialize(filters::AbstractVector{<:AbstractFilter});
     rng::AbstractRNG=default_rng(), 
     optlib::OptLib=optlib,
     igm::IGMAttenuation=Inoue2014IGM())
@@ -98,10 +99,8 @@ function egg(Mstar, z, SF::Bool;
     # Distributions for the bulge-to-total mass ratio
     BT = if SF
         rand(rng, LogNormal(log(exp10(-0.7 + 0.27 * (logMstar - 10))), 0.2 * logten))
-        # exp10(rand(rng, Normal(-0.7 + 0.27 * (logMstar - 10), 0.2)))
     else
         rand(rng, LogNormal(log(exp10(-0.3 + 0.1 * (logMstar - 10))), 0.2 * logten))
-        # exp10(rand(rng, Normal(-0.3 + 0.1 * (logMstar - 10), 0.2)))
     end
     BT = clamp(BT, 0.0, 1.0)
     Mbulge = Mstar * BT
@@ -120,8 +119,9 @@ function egg(Mstar, z, SF::Bool;
     else
         0.9 - 1.3 * log1pz
     end
-    R50_disk = rand(rng, LogNormal(log(exp10(0.2 * (logMstar - 9.35) + Fz_disk)), 0.17 * logten)) # kpc
-    R50_bulge = rand(rng, LogNormal(log(exp10(0.2 * (logMstar - 11.25) + Fz_bulge)), 0.2 * logten)) # kpc
+    R50_disk = exp10(0.2 * (min(10.6, logMstar) - 9.35) + Fz_disk)
+    R50_disk = rand(rng, LogNormal(log(R50_disk), 0.17 * logten)) # kpc
+    R50_bulge = rand(rng, LogNormal(log(exp10(0.56 * (logMstar - 11.25) + Fz_bulge)), 0.2 * logten)) # kpc
     α_R = 1 - 0.8 * log10(R50_disk / R50_bulge)
     BT_α = BT^α_R
     R50_tot = R50_disk * (1 - BT_α) + R50_bulge * BT_α
@@ -138,11 +138,31 @@ function egg(Mstar, z, SF::Bool;
         end
     end
 
-    # Colors
+    # SFRs, initially in log10(sfr) unit
+    ms_disp = 0.3 # Dispersion of SFMS in dex; EGG notes this can change quality of simulation
+    sfrms = logMstar - 9.67 + 1.82 * log1pz - 0.38 * max(0.0, logMstar - 9.59 - 2.22 * log1pz)^2
+    sfr = if SF 
+        rand(rng, Normal(sfrms, ms_disp))
+    else # If passive,
+        rand(rng, Normal(min(sfrms, 0.5 * (logMstar - 11) + log1pz - 0.6), 0.4))
+    end
+    # Add starburst, 3.3% of star-forming galaxies
+    if SF && rand(rng) < 0.033
+        sfr += 0.72 # Enhance SFR by a factor of 5.24
+    end
+    rsb = sfr - sfrms # Ratio of SFR / SFR_MS
+    sfr = exp10(sfr) # Convert to linear units
+    # IRX = log10(L_IR / L_UV)
+    irx = (0.45 * min(z, 3.0) + 0.35) * (logMstar - 10.5) + 1.2
+    irx = exp10(rand(rng, Normal(irx, 0.4)))
+    sfr_ir = sfr / (1 + 1 / irx)
+    sfr_uv = sfr / (1 + irx)
+
+    # Optical colors
     uv_disk, vj_disk = uv_vj(logMstar, z, disk_SF)
     uv_bulge, vj_bulge = uv_vj(logMstar, z, bulge_SF)
 
-    # Optical SEDs; SEDs returned in units of L⊙ / μm / M⊙
+    # Get optical SEDs; SEDs returned in units of L⊙ / μm / M⊙
     m2l_cor = get_m2l_cor(z) # M/L correction in dex
     opt_λ_disk, opt_sed_disk = get_opt_sed(uv_disk, vj_disk, optlib)
     opt_λ_bulge, opt_sed_bulge = get_opt_sed(uv_bulge, vj_bulge, optlib)
@@ -154,7 +174,32 @@ function egg(Mstar, z, SF::Bool;
     opt_λ_disk .*= 1 + z
     opt_λ_bulge .*= 1 + z
 
-    return (Mstar = Mstar, uv_disk = uv_disk, vj_disk = vj_disk, uv_bulge = uv_bulge, vj_bulge = vj_bulge, R50_disk = R50_disk, R50_bulge = R50_bulge, R50 = R50_tot, PA = PA, BT = BT, Mdisk = Mdisk, Mbulge = Mbulge)
+    # IR properties
+    tdust_ms = 32.13 + 4.6 * (z - 2) # T_dust main sequence value at z
+    tdust = tdust_ms + 10.1 * rsb # Starbursts are warmer
+    # Massive galaxies are colder (=downfall of SFE)
+    tdust -= 1.5 * max(0.0, 2 - z) * clamp(logMstar - 10.7, 0.0, 1.0)
+    ir8 = (4.08 + 3.29 * clamp(z - 1, 0.0, 1.0)) * 0.81
+    ir8 *= exp10(0.66 * max(0.0, rsb)) # Starbursts have higher ir8
+    ir8 *= exp10(-clamp(logMstar - 10, -1.0, 0.0)) # low-mass galaxies have larger ir8
+    tdust = rand(rng, Normal(tdust, 0.12 * tdust_ms))
+    ir8 *= exp10(0.18 * randn(rng)) # Ratio of IR to 8 μm luminosity (Elbaz 2011)
+    lir = sfr_ir / 1.72e-10 # Infrared luminosity from 8 to 1000 μm in L⊙
+    ir8 = clamp(ir8, 0.48, 27.5) # range allowed in IR library
+    fpah = clamp(1 / (1 - (331 - 691 * ir8) / (193 - 6.98 * ir8)), 0.0, 1.0)
+    if !isfinite(lir)
+        lir = 0.0
+    end
+
+    # Get IR SED
+    # Merge optical and IR SEDs
+    # Generate emission lines
+    # Add emission lines
+    # Obtain rest-frame magnitudes
+    # Apply IGM absorption, MW dust absorption
+    # Redshift SED, obtain observed magnitudes
+
+    return (Mstar = Mstar, sfr=sfr, sfr_ir = sfr_ir, sfr_uv = sfr_uv, uv_disk = uv_disk, vj_disk = vj_disk, uv_bulge = uv_bulge, vj_bulge = vj_bulge, R50_disk = R50_disk, R50_bulge = R50_bulge, R50 = R50_tot, PA = PA, BT = BT, Mdisk = Mdisk, Mbulge = Mbulge, Tdust = tdust, fpah = fpah, lir = lir, ir8 = ir8, IRX = irx)
 end
 
 # function EGG(Mstar, z; rng::AbstractRNG=default_rng())
