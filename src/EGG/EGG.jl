5a3b7ebe58b79b140402195132106333eab55e50
cgarling <chris.t.garling@gmail.com>
2025-12-04 13:24:25 -0500

    Allow `rng=nothing` to disable randomness

--- src/EGG/EGG.jl
+++ src/EGG/EGG.jl
@@ -6,6 +6,7 @@ Contains code to generate galaxy catalogs using methods similar to those used in
 module EGG
 
 using ..GalaxyGenerator: interp_lin, merge_add
+using ..GalaxyGenerator.IGM: IGMAttenuation, transmission, tau, Inoue2014IGM
 
 using ArgCheck: @argcheck, @check
 using Cosmology: AbstractCosmology, distmod, Planck18
@@ -26,8 +27,6 @@ include("optlib.jl")
 # Load default optical SED library
 const optlib = OptLib()
 const irlib = CS17_IRLib()
-
-include("IGM.jl") # IGM attenuation models
 
 """
     uv_vj(logMstar, z, SF::Bool)
@@ -57,7 +56,7 @@ Returns U-V and V-J colors `(uv, vj)`.
 #         (uv, vj)
 #     end
 # end
-function uv_vj(logMstar, z, SF::Bool, ; rng::AbstractRNG=default_rng())
+function uv_vj(logMstar, z, SF::Bool; rng::Union{AbstractRNG,Nothing}=default_rng())
     # This implementation is based on the code in the egg repository
     uv, vj = if SF
         a0 = 0.58 * erf(logMstar - 10) + 1.39
@@ -66,23 +65,23 @@ function uv_vj(logMstar, z, SF::Bool, ; rng::AbstractRNG=default_rng())
         vj = min(vj, 2.0)
         rnd_amp = 0.2 + (0.25 + 0.12 * clamp((z - 0.5) / 2.0, 0.0, 1.0)) *
                 max(1.0 - 2.0*abs(logMstar - (10.3 + 0.4 * erf(z - 1.5))), 0.0)
-        vj = rand(rng, Normal(vj, rnd_amp))
+        vj = isnothing(rng) ? vj : rand(rng, Normal(vj, rnd_amp))
 
         # Move in UVJ diagram according to UVJ vector
         slope = 0.65
         theta = atan(slope) # This can be simplified but don't care right now
         vj = 0.0 + vj * cos(theta)
         uv = 0.45 + vj * sin(theta)
-        vj = rand(rng, Normal(vj, 0.15))
-        uv = rand(rng, Normal(uv, 0.15))
+        vj = isnothing(rng) ? vj : rand(rng, Normal(vj, 0.15))
+        uv = isnothing(rng) ? uv : rand(rng, Normal(uv, 0.15))
         (uv, vj)
     else
         vj = 0.1 * (logMstar - 11) + 1.25
-        vj = rand(rng, Normal(vj, 0.1)) # Add first error
+        vj = isnothing(rng) ? vj : rand(rng, Normal(vj, 0.1))
         vj = max(min(vj, 1.45), 1.15) # Restrict 1.15 <= V-J <= 1.45
         uv = 0.88 * vj + 0.6
-        vj = rand(rng, Normal(vj, 0.1)) # Add extra error
-        uv = rand(rng, Normal(uv, 0.1)) # Add extra error
+        vj = isnothing(rng) ? vj : rand(rng, Normal(vj, 0.1)) # Add extra error
+        uv = isnothing(rng) ? uv : rand(rng, Normal(uv, 0.1)) # Add extra error
         (uv, vj)
     end
     # Add additional color offset depending on redshift
@@ -95,16 +94,19 @@ end
 # This implementation just generates simple galaxy properties
 # This method is called by the method below that also takes
 # PhotometricFilters and computes magnitudes 
-function egg(Mstar, z, SF::Bool; rng::AbstractRNG=default_rng())
+function egg(Mstar, z, SF::Bool; 
+    rng::Union{Nothing,AbstractRNG}=default_rng())
+
     Mstar, z = Float32(Mstar), Float32(z)
     logMstar = log10(Mstar)
     log1pz = log1p(z) / logten # same as log10(z + 1)
     # Distributions for the bulge-to-total mass ratio
     BT = if SF
-        rand(rng, LogNormal(log(exp10(-0.7 + 0.27 * (logMstar - 10))), 0.2 * logten))
+        exp10(-0.7 + 0.27 * (logMstar - 10))
     else
-        rand(rng, LogNormal(log(exp10(-0.3 + 0.1 * (logMstar - 10))), 0.2 * logten))
+        exp10(-0.3 + 0.1 * (logMstar - 10))
     end
+    BT = isnothing(rng) ? BT : rand(rng, LogNormal(log(BT), 0.2 * logten))
     BT = clamp(BT, 0.0, 1.0)
     Mbulge = Mstar * BT
     Mdisk = Mstar - Mbulge
@@ -122,13 +124,15 @@ function egg(Mstar, z, SF::Bool; rng::AbstractRNG=default_rng())
     else
         0.9 - 1.3 * log1pz
     end
-    R50_disk = exp10(0.2 * (min(10.6, logMstar) - 9.35) + Fz_disk)
-    R50_disk = rand(rng, LogNormal(log(R50_disk), 0.17 * logten)) # kpc
-    R50_bulge = rand(rng, LogNormal(log(exp10(0.56 * (logMstar - 11.25) + Fz_bulge)), 0.2 * logten)) # kpc
+    R50_disk = exp10(0.2 * (min(10.6, logMstar) - 9.35) + Fz_disk) # kpc
+    R50_disk = isnothing(rng) ? R50_disk : rand(rng, LogNormal(log(R50_disk), 0.17 * logten))
+    R50_bulge = exp10(0.56 * (logMstar - 11.25) + Fz_bulge) # kpc
+    R50_bulge = isnothing(rng) ? R50_bulge : rand(rng, LogNormal(log(R50_bulge), 0.2 * logten))
     α_R = 1 - 0.8 * log10(R50_disk / R50_bulge)
     BT_α = BT^α_R
     R50_tot = R50_disk * (1 - BT_α) + R50_bulge * BT_α
-    PA = rand(rng, Uniform(-180, 180)) # Uniform position angle, shared between bulge and disk
+    # Uniform position angle, shared between bulge and disk
+    PA = isnothing(rng) ? 0.0 : rand(rng, Uniform(-180, 180))
 
     disk_SF = SF # Disks are SF if galaxy is SF
     bulge_SF = if ~SF # Bulge is quiescent if galaxy is quiescent
@@ -137,7 +141,7 @@ function egg(Mstar, z, SF::Bool; rng::AbstractRNG=default_rng())
         if BT >= 0.6 # Bulge is quiescient if galaxy is bulge-dominated
             false
         else
-            rand(rng, (true, false)) # Otherwise, 50% chance of quiescent/SF
+            isnothing(rng) ? false : rand(rng, (true, false)) # Otherwise, 50% chance of quiescent/SF
         end
     end
 
@@ -145,38 +149,41 @@ function egg(Mstar, z, SF::Bool; rng::AbstractRNG=default_rng())
     ms_disp = 0.3 # Dispersion of SFMS in dex; EGG notes this can change quality of simulation
     sfrms = logMstar - 9.67 + 1.82 * log1pz - 0.38 * max(0.0, logMstar - 9.59 - 2.22 * log1pz)^2
     sfr = if SF 
-        rand(rng, Normal(sfrms, ms_disp))
+        isnothing(rng) ? sfrms : rand(rng, Normal(sfrms, ms_disp))
     else # If passive,
-        rand(rng, Normal(min(sfrms, 0.5 * (logMstar - 11) + log1pz - 0.6), 0.4))
+        sfr_q = min(sfrms, 0.5 * (logMstar - 11) + log1pz - 0.6)
+        isnothing(rng) ? sfr_q : rand(rng, Normal(sfr_q, 0.4))
+        # rand(rng, Normal(min(sfrms, 0.5 * (logMstar - 11) + log1pz - 0.6), 0.4))
     end
     # Add starburst, 3.3% of star-forming galaxies
-    if SF && rand(rng) < 0.033
+    if SF && !isnothing(rng) && rand(rng) < 0.033
         sfr += 0.72 # Enhance SFR by a factor of 5.24
     end
     rsb = sfr - sfrms # Ratio of SFR / SFR_MS
     sfr = exp10(sfr) # Convert to linear units
     # IRX = log10(L_IR / L_UV)
     irx = (0.45 * min(z, 3.0) + 0.35) * (logMstar - 10.5) + 1.2
-    irx = exp10(rand(rng, Normal(irx, 0.4)))
+    irx = exp10(isnothing(rng) ? irx : rand(rng, Normal(irx, 0.4)))
     sfr_ir = sfr / (1 + 1 / irx)
     sfr_uv = sfr / (1 + irx)
 
     # Optical colors
-    uv_disk, vj_disk = uv_vj(logMstar, z, disk_SF)
-    uv_bulge, vj_bulge = uv_vj(logMstar, z, bulge_SF)
+    uv_disk, vj_disk = uv_vj(logMstar, z, disk_SF; rng=rng)
+    uv_bulge, vj_bulge = uv_vj(logMstar, z, bulge_SF; rng=rng)
 
     # IR properties
     tdust_ms = 32.13 + 4.6 * (z - 2) # T_dust main sequence value at z
     tdust = tdust_ms + 10.1 * rsb # Starbursts are warmer
     # Massive galaxies are colder (=downfall of SFE)
     tdust -= 1.5 * max(0.0, 2 - z) * clamp(logMstar - 10.7, 0.0, 1.0)
-    ir8 = (4.08 + 3.29 * clamp(z - 1, 0.0, 1.0)) * 0.81
-    ir8 *= exp10(0.66 * max(0.0, rsb)) # Starbursts have higher ir8
-    ir8 *= exp10(-clamp(logMstar - 10, -1.0, 0.0)) # low-mass galaxies have larger ir8
-    tdust = rand(rng, Normal(tdust, 0.12 * tdust_ms))
-    ir8 *= exp10(0.18 * randn(rng)) # Ratio of IR to 8 μm luminosity (Elbaz 2011)
-    lir = sfr_ir / 1.72e-10 # Infrared luminosity from 8 to 1000 μm in L⊙
+    tdust = isnothing(rng) ? tdust : rand(rng, Normal(tdust, 0.12 * tdust_ms))
+    # Ratio of IR to 8 μm luminosity (Elbaz 2011)
+    ir8 = log10((4.08 + 3.29 * clamp(z - 1, 0.0, 1.0)) * 0.81)
+    ir8 += 0.66 * max(0.0, rsb) # Starbursts have higher ir8
+    ir8 += -clamp(logMstar - 10, -1.0, 0.0) # low-mass galaxies have larger ir8
+    ir8 = exp10(isnothing(rng) ? ir8 : rand(rng, Normal(ir8, 0.18)))
     ir8 = clamp(ir8, 0.48, 27.5) # range allowed in IR library
+    lir = sfr_ir / 1.72e-10 # Infrared luminosity from 8 to 1000 μm in L⊙
     fpah = clamp(1 / (1 - (331 - 691 * ir8) / (193 - 6.98 * ir8)), 0.0, 1.0)
     if !isfinite(lir)
         lir = 0.0
@@ -190,7 +197,7 @@ function egg(Mstar, z, SF::Bool,
     @nospecialize(filters::AbstractVector{<:AbstractFilter}),
     @nospecialize(mag_sys::AbstractVector{<:MagnitudeSystem});
     cosmo::AbstractCosmology=Planck18,
-    rng::AbstractRNG=default_rng(), 
+    rng::Union{Nothing,AbstractRNG}=default_rng(), 
     optlib::OptLib=optlib,
     irlib::IRLib=irlib,
     igm::IGMAttenuation=Inoue2014IGM())
@@ -239,6 +246,8 @@ function egg(Mstar, z, SF::Bool,
     # Merge optical and IR SEDs
     opt_λ, opt_sed = merge_add(opt_λ_bulge, opt_λ_disk, opt_sed_bulge, opt_sed_disk)
     λ, sed = merge_add(opt_λ, ir_λ, opt_sed, ir_sed)
+    # return λ, sed
+
     # Obtain rest-frame magnitudes
     # This is inefficient as `magnitude` resamples the filter to the λ every time it's called; don't care for now
     mag_abs = [magnitude(filters[i], mag_sys[i], λ * u.μm, sed * u.erg / u.s / u.cm^2 / u.angstrom) for i in eachindex(filters)]
